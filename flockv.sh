258faa7cbd59cf702cad91696039ed378c5ba997
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-10-02 15:11:51 +0800

    Update flockv.sh

--- flockv.sh
+++ flockv.sh
@@ -34,4 +34,4 @@ pip install -r llm-loss-validator/requirements.txt
 cd llm-loss-validator/src
 
 # Run the specified command in the background using tmux
-tmux new-session -d -s flock "CUDA_VISIBLE_DEVICES=0 TIME_SLEEP=180 bash start.sh --hf_token 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 --flock_api_key HH76CARFCS7INRBCIKOKBPSGR42DZ2HM --task_id 12 --validation_args_file validation_config.json.example --auto_clean_cache False --lora_only False &> /root/output.log"
+tmux new-session -d -s flock "CUDA_VISIBLE_DEVICES=0 TIME_SLEEP=180 bash start.sh --hf_token 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 --flock_api_key HH76CARFCS7INRBCIKOKBPSGR42DZ2HM --task_id 12 --validation_args_file validation_config.json.example --auto_clean_cache False --lora_only False >> /root/output.log"
