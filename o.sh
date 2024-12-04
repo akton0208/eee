b931763704a24af1b3f97a648af26b72ee948e92
akton0208 <164739778+akton0208@users.noreply.github.com>
2024-12-04 17:53:08 +0800

    Update o.sh

--- o.sh
+++ o.sh
@@ -1,4 +1,4 @@
-apt-get install openssl pkg-config libssl-dev -y
+apt-get install openssl pkg-config libssl-dev nano screen -y
 apt install -y cargo curl build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
